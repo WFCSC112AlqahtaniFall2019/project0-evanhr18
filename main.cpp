0d4d2bd9e2892b7c8e5f0d7473c50e3006df44ab
evanhr18 <evanhr18@wfu.edu>
2019-09-03 14:09:32 -0400

    added brute force equation solver

--- main.cpp
+++ main.cpp
@@ -2,12 +2,38 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!   " << endl;
-    int x;
 
-    cin >> x;
-    x = x + 10;
-    cout << x;
+    int x1;
+    int y1;
+    int answer1;
+    int x2;
+    int y2;
+    int answer2;
+    int x = -10;
+    int y;
+
+    cin >> x1;
+    cin >> y1;
+    cin >> answer1;
+    cin >> x2;
+    cin >> y2;
+    cin >> answer2;
+
+    while (-10 <= x <= 10) {
+        y = -10;
+        while (-10 <= y <= 10) {
+            if ((x1 * x) + (y1 * y) == answer1 && (x2 * x) + (y1 * y) == answer2) {
+                cout << x << " " << y;
+            }
+            else {
+                y = y+1;
+            }
+        }
+        x = x+1;
+    }
+    cout << "No solution";
+
+
 
     return 0;
 }
